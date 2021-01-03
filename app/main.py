6e7e74e1b3647fc356686e7f5956375193801314
aagasthe <Ananya@Ananyas-MacBook-Pro.local>
2021-01-02 21:48:45 -0800

    Search Call

--- app/main.py
+++ app/main.py
@@ -1,11 +1,15 @@
+import requests
 from flask import Flask, render_template
 app= Flask(__name__)
 
 @app.route('/')
 def index():
-  return "<h1>Welcome to Coding For Shopify</h1>"
+    return "<h1>Welcome to Coding For Shopify</h1>"
 
 @app.route("/search")
 def search():
-  error=None
-  return render_template('search.html', error=error)
+    query = {}
+    response = requests.get('http://www.omdbapi.com/?i=tt3896198&apikey=85a80fa6', params=query)
+    print(response.json())
+    error=None
+    return render_template('search.html', error=error)
