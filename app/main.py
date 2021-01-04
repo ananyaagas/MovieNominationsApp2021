a07bafd3ff0c0529c297369c81484e8934a4e9a4
aagasthe <Ananya@Ananyas-MacBook-Pro.local>
2021-01-03 23:26:49 -0800

    searches based on title and displays search results

--- app/main.py
+++ app/main.py
@@ -1,19 +1,36 @@
 import requests 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, make_response
 app= Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1>Welcome to Coding For Shopify</h1>"
+   return render_template('index.html')
 
 @app.route("/search")
 def search():
+    text = request.args.get('jsdata')
+    #TODO: if text is empty don't return anything
     query = {}
     results = []
-    response = requests.get('http://www.omdbapi.com/?s=guardians&apikey=85a80fa6', params=query)
+    response = requests.get('http://www.omdbapi.com/?s={}&apikey=85a80fa6'.format(text), params=query)
     print(response.json())
     results = response.json() 
     error=None
-    return render_template('search.html', error=error, results=results)
+    username = request.cookies.get('username')
 
+    return render_template('search.html', error=error, results=results, username=username)
 
+@app.route('/setcookie', methods = ['POST', 'GET'])
+def setcookie():
+   if request.method == 'POST':
+       user = request.form['nm']
+   
+   resp = make_response(render_template('readcookie.html'))
+   resp.set_cookie('username', user)
+
+   return resp
+
+@app.route('/getcookie')
+def getcookie():
+   name = request.cookies.get('username')
+   return '<h1>welcome ' + name + '</h1>'
